28ec03dd21cd08ff0fecaf71c5b409442b8183ba
Prem <premkhodke.99@gmail.com>
2024-06-20 18:35:31 +0530

    index setup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const dbConnect = require('./db/dbConfig');
+
+dbConnect();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use('/', (req, res) => {
     res.send('Hello')
 });
 
 app.listen(5000, () => {
-    console.log('server started');
-})
+    console.log('server started', 5000);
+});

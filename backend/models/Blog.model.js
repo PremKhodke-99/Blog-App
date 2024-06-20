47ec7c248882482edfe2fd76ba26d5446e939b45
Prem <premkhodke.99@gmail.com>
2024-06-20 18:35:04 +0530

    database schemas

---
+++ backend/models/Blog.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Blog = model('Blog', blogSchema);
+module.exports = Blog;

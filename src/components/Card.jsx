2578259de14c45aea23e248dd73c5df030573be0
Prem <premkhodke.99@gmail.com>
2024-06-21 21:50:01 +0530

    blog displaying card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { GrView } from 'react-icons/gr'
 import { MdDeleteSweep, MdEditNote } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
-const Card = ({ user, blog, handleDelete, handleEdit }) => {
+const Card = ({ user, blog, handleDelete, handleEdit, handleView }) => {
+
 
     return (
         <div className='w-[32%] max-h-64 border-2 border-solid border-indigo-100 p-4 rounded-xl shadow-lg flex flex-col gap-3 overflow-hidden'>
             <h1 className='text-2xl font-semibold'>{blog?.title}</h1>
             <p className='text-lg text-ellipsis overflow-hidden'>{blog?.content}</p>
+            <p className='text-sm text-slate-600'>Author: {blog.author}</p>
             <div className='flex gap-2 justify-end items-end'>
+                <button
+                    onClick={() => handleView(blog)}
+                    className='w-10 h-10 bg-green-600 text-white rounded-lg flex justify-center items-center text-2xl hover:bg-green-700'
+                >
+                    <GrView />
+                </button>
                 {
                     user && (<>
                         <button

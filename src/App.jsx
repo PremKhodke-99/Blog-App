59179721cd9b078b54d246f69a7764adc619f5ae
Prem <premkhodke.99@gmail.com>
2024-06-21 16:33:38 +0530

    Entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import Homepage from "./components/Homepage"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar"
+import Signup from "./components/Signup"
+import AddBlog from "./components/AddBlog"
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+
 
 function App() {
 
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('loggedInUser')));
+  }, [])
+
+  const updateUser = (user) => {
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+    setUser(user);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('loggedInUser');
+    setUser({});
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-[100vw] h-[100vh] overflow-x-hidden">
+      <Navbar user={user} handleLogout={handleLogout} />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Homepage user={user} />} />
+          <Route path="/login" element={!user ? <Login updateUser={updateUser} /> : <Homepage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/add-blog" element={!user ? <Login updateUser={updateUser} /> : <AddBlog />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 

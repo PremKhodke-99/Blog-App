05b4dbe5709c581c4dde8df2ea91504047f80fb6
Prem <premkhodke.99@gmail.com>
2024-06-21 21:51:09 +0530

    view Blog Page

---
+++ src/components/ViewBlog.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const ViewBlog = ({ user }) => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    console.log(location.state);
+
+    return (
+        <div className='p-4 h-[80%] relative'>
+            <h1 className='text-3xl mb-5 font-bold text-center'>{location.state.blog.title}</h1>
+            <p className='text-xl p-2 italic'>{location.state.blog.content}</p>
+            <p className='text-sm mt-4 p-2'>Author: {location.state.blog.author}</p>
+
+            {/* {
+                user && <div className='absolute bottom-0 left-[50%] -translate-x-1/2 grid grid-cols-2  gap-2'>
+                    <button className='w-24 h-auto p-2 bg-indigo-600 text-white rounded-lg flex justify-center items-center text-xl hover:bg-indigo-700'>Edit</button>
+                    <button className='w-24 h-auto p-2 bg-red-600 text-white rounded-lg flex justify-center items-center text-xl hover:bg-red-700'>Delete</button>
+                </div>
+            } */}
+        </div>
+    )
+}
+
+export default ViewBlog

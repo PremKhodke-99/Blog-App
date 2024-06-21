4fb7e579b2d238620cd9fdb71a32f0a63e0a520b
Prem <premkhodke.99@gmail.com>
2024-06-21 21:24:58 +0530

    /:id route

--- backend/routes/blog.routes.js
+++ backend/routes/blog.routes.js
@@ -1,8 +1,9 @@
-const { getAllBlogs, addNewBlog, editBlog, deleteBlog } = require('../controller/blog.controller');
+const { getAllBlogs, addNewBlog, editBlog, deleteBlog, getBlogById } = require('../controller/blog.controller');
 
 const router = require('express').Router();
 
 router.get('/', getAllBlogs);
+router.get('/getById/:id', getBlogById);
 router.post('/addBlog', addNewBlog);
 router.put('/editBlog/:id', editBlog)
 router.delete('/deleteBlog/:id', deleteBlog);
